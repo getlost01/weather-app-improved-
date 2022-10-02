7f4014d43f38dbc5a00f236c59c1eeb67d6fe293
Rishabh <rishabhverma.1202@gmail.com>
2022-10-02 21:18:44 +0530

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const request = require('request');
 const argv = require('yargs').argv;
 
-let apiKey = 'a09ce5ac8ae4418b30579684f8253ead';
+let apiKey = '41898b7881fc62d5a199b9f3f54b3b38';
 let city = argv.c || 'Delhi';
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
